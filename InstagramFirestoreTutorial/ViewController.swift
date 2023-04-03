7ff77cfcbfa486fe32fe2b3d5f3e731a5f4edfa9
Seikendojo <94020124+Seikendojo@users.noreply.github.com>
2023-04-02 23:05:01 -0700

    Initial Commit

---
+++ InstagramFirestoreTutorial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstagramFirestoreTutorial
+//
+//  Created by Seiken Dojo on 2023-04-02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
