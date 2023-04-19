3d8b85fae9df15c005cda46fbdeb80e8b6fc99dd
Seikendojo <94020124+Seikendojo@users.noreply.github.com>
2023-04-19 14:06:04 -0700

    Add Profile CollectionView data source.

--- InstagramFirestoreTutorial/Controller/ProfileController.swift
+++ InstagramFirestoreTutorial/Controller/ProfileController.swift
@@ -29,3 +29,36 @@ class ProfileController: UICollectionViewController {
     }
 
 }
+
+
+//MARK: - UICollectionViewDataSource
+
+extension ProfileController {
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 9
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellIdentifier, for: indexPath) as! ProfileCell 
+        return cell
+    }
+    
+}
+
+
+
+
+
+//MARK: - UICollectionViewDelegate
+
+extension ProfileController {
+    
+}
+
+
+
+//MARK: - UICollectionViewDelegateFlowLayout
+
+extension ProfileController: UICollectionViewDelegateFlowLayout {
+    
+}
