b523cbe327a2b79a402366aa96dac20f23eb9c01
Seikendojo <94020124+Seikendojo@users.noreply.github.com>
2023-04-22 11:45:20 -0700

    Add profile cell and profile header imageView. add name label. Implement edit profile follow button.

--- InstagramFirestoreTutorial/View/Profile/ProfileCell.swift
+++ InstagramFirestoreTutorial/View/Profile/ProfileCell.swift
@@ -11,14 +11,22 @@ class ProfileCell: UICollectionViewCell {
     
     //MARK: - Properties
     
-    
+    private var postImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.image = UIImage(named: "venom-7")
+        iv.contentMode = .scaleToFill
+        iv.clipsToBounds = true
+        return iv
+    }()
     
     //MARK: - Lifecycle
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        
+    
         backgroundColor = .lightGray
+        addSubview(postImageView)
+        postImageView.fillSuperview()
     }
     
     required init?(coder: NSCoder) {
--- InstagramFirestoreTutorial/View/Profile/ProfileHeader.swift
+++ InstagramFirestoreTutorial/View/Profile/ProfileHeader.swift
@@ -13,6 +13,32 @@ class ProfileHeader: UICollectionReusableView {
     
     //MARK: - Properties
     
+    private let profileImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.image = UIImage(named: "venom-7")
+        iv.contentMode = .scaleToFill
+        iv.clipsToBounds = true
+        return iv
+    }()
+    
+    private let nameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Eddie Brock"
+        label.font = UIFont.systemFont(ofSize: 14)
+        return label
+    }()
+    
+    private lazy var editProfileFollowButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Edit Profile", for: .normal)
+        button.layer.cornerRadius = 3
+        button.layer.borderColor = UIColor.lightGray.cgColor
+        button.layer.borderWidth = 0.5
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        button.setTitleColor(.black, for: .normal)
+        button.addTarget(self, action: #selector(handleEditProfileFollowTapped), for: .touchUpInside)
+        return button
+    }()
     
     
     //MARK: - Lifecycle
@@ -20,12 +46,31 @@ class ProfileHeader: UICollectionReusableView {
     override init(frame: CGRect) {
         super.init(frame: frame)
         
-        backgroundColor = .systemPink
+        backgroundColor = .white
+        addSubview(profileImageView)
+        profileImageView.anchor(top: topAnchor, left: leftAnchor,paddingTop: 16, paddingLeft: 12)
+        profileImageView.setDimensions(height: 80, width: 80)
+        profileImageView.layer.cornerRadius = 80 / 2
+        
+        addSubview(nameLabel)
+        nameLabel.anchor(top: profileImageView.bottomAnchor, left: leftAnchor, paddingTop: 12 , paddingLeft: 12)
+        
+        addSubview(editProfileFollowButton)
+        editProfileFollowButton.anchor(top: nameLabel.bottomAnchor, left: leftAnchor,
+                                       right: rightAnchor,paddingTop: 16,
+                                       paddingLeft: 24, paddingRight: 24)
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    //MARK: - Actions
+    
+    @objc func handleEditProfileFollowTapped() {
+        print("DEBUG: Handle edit profile tapped....")
+    }
+
     
 
 }
