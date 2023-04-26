1b4e6c517dafa77edbca07fdaba902b35e37f9eb
Seikendojo <94020124+Seikendojo@users.noreply.github.com>
2023-04-26 12:44:02 -0700

    Add Profile Header ViewModel.

--- InstagramFirestoreTutorial/Controller/ProfileController.swift
+++ InstagramFirestoreTutorial/Controller/ProfileController.swift
@@ -15,7 +15,7 @@ class ProfileController: UICollectionViewController {
     //MARK: - Properties
     
     var user: User? {
-        didSet { navigationItem.title = user?.username }
+        didSet { collectionView.reloadData() }
     }
 
     override func viewDidLoad() {
@@ -29,6 +29,7 @@ class ProfileController: UICollectionViewController {
     func fetchUser() {
         UserService.fetchUser { user in
             self.user = user
+            self.navigationItem.title = user.username
         }
     }
 
@@ -59,10 +60,13 @@ extension ProfileController {
     
     override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
         let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: headerIdentifier, for: indexPath) as! ProfileHeader
-        return header
         
+        if let user = user {
+            header.viewModel = ProfileHeaderViewModel(user: user)
+        }
+        
+        return header
     }
-    
 }
 
 
