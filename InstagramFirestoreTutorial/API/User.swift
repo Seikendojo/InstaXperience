f6030ec4c633382a590ee48b56075650e9a2cf80
Seikendojo <94020124+Seikendojo@users.noreply.github.com>
2023-05-16 14:14:52 -0700

    Implement unfollowing user & updating UI. Check if user is unfollowed. Add UserStats functionality.

--- InstagramFirestoreTutorial/API/User.swift
+++ InstagramFirestoreTutorial/API/User.swift
@@ -18,6 +18,8 @@ struct User {
     
     var isFollowed = false
     
+    var stats: UserStats!
+    
     var isCurrentUser: Bool { return Auth.auth().currentUser?.uid == uid }
     
     init(dictionary: [String: Any]) {
@@ -27,6 +29,13 @@ struct User {
         self.username = dictionary["username"] as? String ?? ""
         self.uid = dictionary["uid"] as? String ?? ""
         
+        self.stats = UserStats(followers: 0, following: 0)
+        
     }
-    
+}
+
+struct UserStats {
+    let followers: Int
+    let following: Int
+   // let posts: Int
 }
