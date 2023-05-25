2056e9796ff47e02751acc77f66e80d8c71bd71b
Seikendojo <94020124+Seikendojo@users.noreply.github.com>
2023-05-24 20:43:13 -0700

    Add sorting post functionality to PostService. Update UploadPostControllerDelegate in MainTabController to handle refresh.

--- InstagramFirestoreTutorial/Controller/MainTabController.swift
+++ InstagramFirestoreTutorial/Controller/MainTabController.swift
@@ -135,6 +135,10 @@ extension MainTabController: UploadPostControllerDelegate {
     func controllerDidFinishUploadingPost(_ controller: UploadPostController) {
         selectedIndex = 0
         controller.dismiss(animated: true)
+        
+        guard let feedNav = viewControllers?.first as? UINavigationController else { return }
+        guard let feed = feedNav.viewControllers.first as? FeedController else { return }
+        feed.handleRefresh()
     }
 }
 
