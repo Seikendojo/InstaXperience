ddf912d493b904840ae3bf5b3ed71245ad224a79
Seikendojo <94020124+Seikendojo@users.noreply.github.com>
2023-06-17 16:59:08 -0700

    Create commentLabelText function and size function in CommentViewModel. Create configure function in CommentCell to be able to add profileImageView and commentLabel attributedText. Populate CollectionView with cells with CommentViewModel.

--- InstagramFirestoreTutorial/View/CommentCell.swift
+++ InstagramFirestoreTutorial/View/CommentCell.swift
@@ -12,6 +12,10 @@ class CommentCell: UICollectionViewCell {
     
     //MARK: - Properties
     
+    var viewModel: CommentViewModel? {
+        didSet { configure() }
+    }
+    
     private let profileImageView: UIImageView = {
         let iv = UIImageView()
         iv.contentMode = .scaleAspectFill
@@ -20,13 +24,7 @@ class CommentCell: UICollectionViewCell {
         return iv
     }()
     
-    private let commentLabel: UILabel = {
-        let label = UILabel()
-        let attributedString = NSMutableAttributedString(string: "kash ", attributes: [.font: UIFont.boldSystemFont(ofSize: 14)])
-        attributedString.append(NSAttributedString(string: "some test comments for now..", attributes: [.font: UIFont.systemFont(ofSize: 14)]))
-        label.attributedText = attributedString
-        return label
-    }()
+    private let commentLabel = UILabel()
     
     //MARK: - Lifecycle
     
@@ -39,13 +37,25 @@ class CommentCell: UICollectionViewCell {
         profileImageView.setDimensions(height: 40, width: 40)
         profileImageView.layer.cornerRadius = 40 / 2
         
+        commentLabel.numberOfLines = 0
+        
         addSubview(commentLabel)
         commentLabel.centerY(inView: profileImageView,
                              leftAnchor: profileImageView.rightAnchor,
                              paddingLeft: 8, constant: 0)
+        commentLabel.anchor(right: rightAnchor, paddingRight: 8)
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    //MARK: - Helpers
+    
+    func configure() {
+        guard let viewModel = viewModel else { return }
+        
+        profileImageView.sd_setImage(with: viewModel.profileImageUrl)
+        commentLabel.attributedText = viewModel.commentLabelText()
     }
 }
--- InstagramFirestoreTutorial/ViewModel/CommentViewModel.swift
+++ InstagramFirestoreTutorial/ViewModel/CommentViewModel.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import UIKit
 
 struct CommentViewModel {
     
@@ -13,11 +14,23 @@ struct CommentViewModel {
     
     var profileImageUrl: URL? { return URL(string: comment.profileImageUrl) }
     
-    var username: String { return comment.username }
-    
-    var commentText: String { return comment.commentText  }
-    
     init(comment: Comment) {
         self.comment = comment
+    }
+    
+    func commentLabelText() -> NSAttributedString {
+        let attributedString = NSMutableAttributedString(string: "\(comment.username)", attributes: [.font: UIFont.boldSystemFont(ofSize: 14)])
+        attributedString.append(NSAttributedString(string: " \(comment.commentText)", attributes: [.font: UIFont.systemFont(ofSize: 14)]))
+        
+        return attributedString
+    }
+    
+    func size(forWidth width: CGFloat) -> CGSize {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.text = comment.commentText
+        label.lineBreakMode = .byWordWrapping
+        label.setWidth(width)
+        return label.systemLayoutSizeFitting(UIView.layoutFittingCompressedSize)
     }
 }
