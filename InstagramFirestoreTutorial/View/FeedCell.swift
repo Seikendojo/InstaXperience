77db432bcde5cd043af50a87bc233a84337bbb97
Seikendojo <94020124+Seikendojo@users.noreply.github.com>
2023-06-25 13:09:36 -0700

    Add likePost function to PostService API. Call the likePost function in FeedController. Make likeButton public to get access to.

--- InstagramFirestoreTutorial/View/FeedCell.swift
+++ InstagramFirestoreTutorial/View/FeedCell.swift
@@ -49,7 +49,7 @@ class FeedCell: UICollectionViewCell {
         return iv
     }()
     
-    private lazy var likeButton: UIButton = {
+     lazy var likeButton: UIButton = {
         let button = UIButton(type: .system)
         button.setImage(UIImage(named: "like_unselected"), for: .normal)
         button.tintColor = .black
